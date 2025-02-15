138af2a5b477eba991156a6444d17ceeca08fd1b
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-02-16 02:35:04 +0530

    main video background static

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -5,12 +5,12 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId)
 
   const { trailerVideo } = useSelector((store) => store.movies)
-
+  console.log('trailerVideo', trailerVideo)
   return (
     <div>
       <iframe
         style={{ width: '100vw', aspectRatio: '16 / 9' }}
-        src={`https://www.youtube.com/embed/${trailerVideo}?autoplay=1&mute=1`}
+        src={`https://www.youtube.com/embed/hR1-ihzff3I?autoplay=1&mute=1`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
